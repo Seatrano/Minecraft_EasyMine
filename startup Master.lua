bb481ab49560af5e8b58fcc2c9a466fc13414e0e
Max Ellinger <maxellingerjonas@gmail.com>
2025-11-27 18:35:19 +0100

    Create startup Master.lua

---
+++ startup Master.lua
@@ -0,0 +1,28 @@
+-- startup.lua
+local url = "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/refs/heads/main/Master.lua"
+local fileName = "Computer"  -- die Datei, die ausgeführt wird
+
+-- Versuche, die aktuelle Datei von GitHub zu laden
+if http then
+    local response = http.get(url)
+    if response then
+        local content = response.readAll()
+        response.close()
+        -- Überschreibe die aktuelle startup Datei
+        local f = fs.open(fileName, "w")
+        f.write(content)
+        f.close()
+        print("Startup erfolgreich aktualisiert.")
+    else
+        print("Fehler: Konnte die Datei nicht herunterladen.")
+    end
+else
+    print("HTTP API ist deaktiviert! Setze 'enable_http' in den CC-Einstellungen.")
+end
+
+-- Starte die Datei
+if fs.exists(fileName) then
+    shell.run(fileName)
+else
+    print("Fehler: Startup-Datei nicht gefunden.")
+end
