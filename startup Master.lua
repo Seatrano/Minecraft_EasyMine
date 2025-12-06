b0976758c8c4c6b0b70e9a1704ea99d59bb5f904
Max Ellinger <maxellingerjonas@gmail.com>
2025-12-06 14:23:08 +0100

    Update startup Master.lua

--- startup Master.lua
+++ startup Master.lua
@@ -1,28 +1,26 @@
--- startup.lua
-local url = "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/Master.lua?" .. os.time()
-local fileName = "Master.lua"  -- die Datei, die ausgeführt wird
+local url = "https://api.github.com/repos/Seatrano/Minecraft_EasyMine/contents/Master.lua"
 
--- Versuche, die aktuelle Datei von GitHub zu laden
-if http then
-    local response = http.get(url)
-    if response then
-        local content = response.readAll()
-        response.close()
-        -- Überschreibe die aktuelle startup Datei
-        local f = fs.open(fileName, "w")
-        f.write(content)
-        f.close()
-        print("Startup erfolgreich aktualisiert.")
-    else
-        print("Fehler: Konnte die Datei nicht herunterladen.")
-    end
+local request = http.get(url, {
+    ["User-Agent"] = "CC-HTTP"
+})
+
+if request then
+    local data = textutils.unserializeJSON(request.readAll())
+    request.close()
+
+    local decoded = textutils.unserializeJSON(
+        '{"content":"' .. data.content .. '","encoding":"base64"}'
+    ).content
+
+    local content = textutils.decode_base64(data.content)
+
+    local f = fs.open("Master.lua", "w")
+    f.write(content)
+    f.close()
+
+    print("Aktualisiert über GitHub API.")
 else
-    print("HTTP API ist deaktiviert! Setze 'enable_http' in den CC-Einstellungen.")
+    print("Fehler beim Laden über API.")
 end
 
--- Starte die Datei
-if fs.exists(fileName) then
-    shell.run(fileName)
-else
-    print("Fehler: Startup-Datei nicht gefunden.")
-end
+shell.run("Master.lua")
