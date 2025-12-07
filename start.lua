037855b1472d06907d42aa3e6bc7df0bfdaae75a
Max Ellinger <maxellingerjonas@gmail.com>
2025-12-07 18:40:22 +0100

    Update start.lua

--- start.lua
+++ start.lua
@@ -56,16 +56,16 @@ end
 -- Programme laden und starten
 if selection == "GPSMaster" then
     downloadIfMissing("startup GPSMaster.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20GPS.lua")
-    shell.run("startup GPSMaster.lua")
+    shell.run("GPSMasterStart.lua")
 elseif selection == "Master" then
-    downloadIfMissing("startup Master.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20Master.lua")
-    shell.run("startup Master.lua")
+    downloadIfMissing("MasterStart.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20Master.lua")
+    shell.run("MasterStart.lua")
 elseif selection == "Slave" then
-    downloadIfMissing("startup Slave.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20Slave.lua")
-    shell.run("startup Slave.lua")
+    downloadIfMissing("SlaveStart.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20Slave.lua")
+    shell.run("SlaveStart.lua")
 elseif selection == "Debug" then
-    downloadIfMissing("startup Debug.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20Debug.lua")
-    shell.run("startup Debug.lua")
+    downloadIfMissing("DebugStart.lua", "https://raw.githubusercontent.com/Seatrano/Minecraft_EasyMine/main/startup%20Debug.lua")
+    shell.run("DebugStart.lua")
 
 else
     error("Unknown selection: " .. selection)
